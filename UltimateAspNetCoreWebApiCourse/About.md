6deb1b143d6e184a49afe07c9d12369cfef39171
Anderson Urbano <anderson.urb01@gmail.com>
2021-08-31 20:32:03 -0300

    chapter 1

---
+++ UltimateAspNetCoreWebApiCourse/About.md
@@ -0,0 +1,11 @@
+# Ultimate ASP.NET Core Web API
+
+## Link
+https://code-maze.com/ultimate-aspnet-core-web-api/
+
+## Instructor
+Marinko Spasojevic, Vladimir Pecanac
+
+## Description
+
+A complete, hands-on, and easy to digest program that gets you from zero to building production-ready APIs in just a few weeks
---
+++ UltimateAspNetCoreWebApiCourse/UltimateAspNetCoreWebApiCourse/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltimateAspNetCoreWebApiCourse.Extensions;
+
+namespace UltimateAspNetCoreWebApiCourse
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
