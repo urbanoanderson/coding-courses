f7fd2278669977b385616bae03dc7831eb3cc1c3
Anderson Urbano <anderson.urb01@gmail.com>
2021-09-11 18:33:56 -0300

    chapter 21

---
+++ UltimateAspNetCoreWebApiCourse/Entities/LinkModels/Link.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.LinkModels
+{
+    public class Link
+    {
+        public Link()
+        {
+            // Only for XML serialization purposes
+        }
+
+        public Link(string href, string rel, string method)
+        {
+            this.Href = href;
+            this.Rel = rel;
+            this.Method = method;
+        }
+
+        public string Href { get; set; }
+
+        public string Rel { get; set; }
+
+        public string Method { get; set; }
+    }
+}
